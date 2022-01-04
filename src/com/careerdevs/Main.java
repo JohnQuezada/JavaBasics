57af725a756650ff1eb67695f85e6a9d1427b733
JohnQuezada <johnaquezada@gmail.com>
2022-01-03 20:33:26 -0500

    part 1/3

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -3,6 +3,9 @@ package com.careerdevs;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	String firstName = "John";
+    String lastName = "Quezada";
+    String fullName = firstName + ' ' + lastName;
+        System.out.println(fullName);
     }
 }
