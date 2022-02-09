fa75550316484cedaacc47c6d264fd6be04e9826
JohnQuezada <johnaquezada@gmail.com>
2022-02-08 19:42:17 -0500

    countdown update

--- src/com/careerdevs/introtorecursion/Main.java
+++ src/com/careerdevs/introtorecursion/Main.java
@@ -1,8 +1,12 @@
 package com.careerdevs.introtorecursion;
 
+
+// recursion: is when a method calls itself
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(sum(2));
+        //System.out.println(sum(2));
+        System.out.println(countDown(5));
     }
 
     // edabit challenge
@@ -11,5 +15,11 @@ public class Main {
             return sum(n - 1) + n;
         }
         return n;
+    }
+
+    public static int countDown(int n) {
+        for (int i = n; i >= 1 ; i--) {
+            System.out.println(i);
+        } return n;
     }
 }
